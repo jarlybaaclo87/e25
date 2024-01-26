a546bb30f852f8a6f2df68d1cacd92ab7cd6bb54
Jarly Love Baaclo <jlbaaclo87@gmail.com>
2024-01-26 14:09:49 +0800

    update index.html stack.js style.css

--- stack.js
+++ stack.js
@@ -1,72 +1,76 @@
-
 class Stack {
     constructor() {
         this.items = []
         this.count = 0
-         
     }
-
     // Add element to top of stack
-    push(element) {
-        this.items[this.count] = element
-        let push_res=`${element}`; //console.log(`added to ${this.count}`);
-        this.count += 1;
-        let count=this.count -1;
-        //return [push_res,count];
-        return [push_res, count];
+    push(Element){
+        this.items[this.count] = Element
+        console.log(`${Element} added to ${this.count}`)
+        this.count += 1
+        return this.count - 1
     }
+   //  Return and remove top element in stack
+   // Return undefined if is empty
+   pop() {
+    if(this.count == 0) return undefined
+    let deleteItem = this.items[this.count - 1]
+    this.count -= 1
+    console.log(`${deleteItem} removed`)
+    return deleteItem
+   }
+   // Check top element in stack
+   peek() {
+    console.log(`Top element is ${this.items[this.count - 1]}`)
+    return this.items[this.count - 1]
+   }
 
-    // Return and remove top element in stack
-    // Return undefined if stack is empty
-    pop() {
-        if(this.count == 0) return undefined
-        let deleteItem = this.items[this.count - 1]
-        this.count -= 1
-        console.log(`${deleteItem} removed`)
-        return deleteItem
-    }
+   // Check  if stack is empty
+   isEmpty() {
+    console.log(this.count == 0 ? 'Stack is empty' : 'Stack is NOT empty')
+    return this.count == 0
+   }
 
-    // Check top element in stack
-    peek() {
-        console.log(`Top element is ${this.items[this.count - 1]}`)
-        return this.items[this.count - 1]
-    }
+   // Check size of stack 
+   size() {
+    console.log(`${this.count} elements in stack`)
+    return this.count
+   }
 
-    // Check if stack is empty
-    isEmpty() {
-        console.log(this.count == 0 ? 'Stack is empty' : 'Stack is NOT empty')
-        return this.count == 0
-    }
-
-    // Check size of stack
-    size() {
-        console.log(`${this.count} elements in stack`)
-        return this.count
-    }
-
-    // Print elements in stack
-    print() {
-        let str = ''
-        for(let i = 0; i < this.count; i++) {
+   // Print element in stack
+   print() {
+     let str = ''
+     for(let i = 0; i < this.count; i++){
             str += this.items[i] + ' '
-        }
-        return str
-    }
+     }
+     return str
+   }
 
-    // Clear stack
-    clear() {
-        this.items = []
-        this.count = 0
-        console.log('Stack cleared..')
-        return this.items
-    }
+   // Clear stack
+   clear(){
+    this.items = []
+    this.count = 0
+    console.log(`Stack cleared..`)
+    return this.items
+   }
 }
 
 const stack = new Stack()
+stack.isEmpty()
 
-$(document).ready(function(){
-    $("#stack").prepend(`<div class="row bg-primary push">${stack.push(100)[0]}</div>`);
-    $("#stack").prepend(`<div class="row bg-success push">${stack.push(200)[0]}</div>`);
-    $("#stack").prepend(`<div class="row bg-primary push">${stack.push(300)[0]}</div>`);
-    $("#stack").prepend(`<div class="row bg-success push">${stack.push(400)[0]}</div>`);
-});
+stack.push(100)
+stack.push(200)
+
+stack.peek()
+stack.push(300)
+
+console.log(stack.print())
+
+stack.pop()
+stack.pop()
+
+stack.clear()
+
+console.log(stack.print())
+stack.size()
+stack.isEmpty()
